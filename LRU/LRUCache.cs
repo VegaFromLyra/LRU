e2cfd78e3a6de512fd7e5575fe048af92217794e
VegaFromLyra <asha.ab@gmail.com>
2014-03-21 20:14:54 -0700

    LRU cache

---
+++ LRU/LRUCache.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LRU
+{
+    public class Storage<T>
+    {
+        private LinkedList<Node<T>> list = new LinkedList<Node<T>>();
+
+        private Dictionary<string, LinkedListNode<Node<T>>> table =
+                    new Dictionary<string, LinkedListNode<Node<T>>>();
+
+        public Storage(int capacity)
+        {
+            Capacity = capacity;
+        }
+
+        public int Capacity { get; private set; }
+
+        public void Put(string key, T value)
+        {
+            if (table.ContainsKey(key))
+            {
+                UpdateExistingNode(key, value);
+            }
+            else
+            {
+                if (table.Count >= Capacity)
+                {
+                    table.Remove(list.Last.Value.Key);
+                    list.RemoveLast();
+                }
+
+                AddNodeToFront(key, value);
+            }
+        }
+
+        // Get the node and move it to front
+        // return value associate with node
+        public T Get(string key)
+        {
+            if (!table.ContainsKey(key))
+            {
+                return default(T);
+            }
+
+            LinkedListNode<Node<T>> linkedListNode = table[key];
+
+            T result = linkedListNode.Value.Value;
+
+            list.Remove(linkedListNode);
+            list.AddFirst(linkedListNode);
+
+            return result;
+        }
+
+        // Creates a new node in the linked list
+        // Adds reference of this node the dictionary
+        private void AddNodeToFront(string key, T value)
+        {
+            Node<T> newNode = new Node<T>(key, value);
+            LinkedListNode<Node<T>> newLinkedListNode = list.AddFirst(newNode);
+            table.Add(key, newLinkedListNode);
+        }
+
+        private void UpdateExistingNode(string key, T value)
+        {
+            LinkedListNode<Node<T>> existingLinkedListNode = table[key];
+            existingLinkedListNode.Value.Value = value;
+
+            // Move node to front of list if it is 
+            // not at the start already
+            if (list.First != existingLinkedListNode)
+            {
+                list.Remove(existingLinkedListNode);
+                list.AddFirst(existingLinkedListNode);
+            }
+        }
+    }
+
+
+    public class Node<T>
+    {
+        public Node(string key, T value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public string Key { get; set; }
+
+        public T Value { get; set; }
+    }
+}
---
+++ LRU/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LRU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Storage<int> cache = new Storage<int>(3);
+
+            // 3 - 2 - 1
+            // 4 - 3 - 2
+            // 2 - 4 - 3
+            // 4 - 2 - 3
+            // 3 - 4 - 2
+            // 6 - 5 - 3
+
+            cache.Put("gappu", 1);
+            cache.Put("Tecy", 3);
+            cache.Put("Amma", 5);
+            cache.Put("gappu", 6);
+            cache.Put("Tecy", 4);
+            cache.Put("Andy", 2);
+
+            // Andy -> Tecy -> gappu
+
+            Test(cache, "gappu", 6);
+            Test(cache, "Tecy", 4);
+            Test(cache, "Andy", 2);
+        }
+
+        static void Test(Storage<int> storage, string key, int expectedValue)
+        {
+            Console.WriteLine("Expected: {0}, Actual: {1}", expectedValue, storage.Get(key));
+        }
+    }
+}
